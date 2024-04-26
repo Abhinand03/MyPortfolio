9d76519131b22f0602eb7e185fc0da7875b44a06
Abhinand03 <abhinandpk0123@gmail.com>
2024-04-26 09:28:31 +0530

    first

---
+++ main.js
@@ -0,0 +1,51 @@
+
+const ch_text = document.querySelector(".ch-text")
+
+const textload=()=>{
+    setTimeout(()=>{
+        ch_text.textContent="Back-end developer"
+    },0);
+    setTimeout(()=>{
+        ch_text.textContent="Full-stack devloper"
+    },1000);
+    setTimeout(()=>{
+        ch_text.textContent="front-end developer"
+    },2000);
+}
+
+textload()
+setInterval(textload,1000)
+ScrollReveal({
+
+    reset: true,
+    distance:'10px',
+    duration:1500,
+    delay:400
+  })
+
+ScrollReveal().reveal('.timeline', { interval: 500 });
+ScrollReveal().reveal('.s0', { interval: 100 });
+ScrollReveal().reveal('.m-intro', { delay: 500, origin: 'right', enter: 'center' });
+ScrollReveal().reveal('.img-div', { delay: 500, origin: 'left', enter: 'bottom' });
+// ScrollReveal().reveal('.card1', { delay: 500, origin: 'left', enter: 'bottom' });
+// ScrollReveal().reveal('.sc-car', { delay: 500, origin: 'right', enter: 'bottom' });
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+
+
