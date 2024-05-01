081f06e0cf582917bfb11136e50c691473e5bbbd
Abhinand03 <abhinandpk0123@gmail.com>
2024-05-01 10:27:00 +0530

    second

--- main.js
+++ main.js
@@ -48,4 +48,20 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
+$("#submit-form").submit((e)=>{
+    e.preventDefault()
+    $.ajax({
+        url:"https://script.google.com/macros/s/AKfycbzBFiLcl-HqYzBzrrd_obFvIHIKtP1Bg2qNhrCDpsYcSKh0oAbCKXeIzRbcKvP7bDzp/exec",
+        data:$("#submit-form").serialize(),
+        method:"POST",
+        success:function (response){
+            alert("Form submitted successfully")
+            window.location.reload()
+            //window.location.href="https://google.com"
+        },
+        error:function (err){
+            alert("Something Error")
 
+        }
+    })
+})
